d68dc4a7cea7e275dcb2f1f19c4cba529aa0cd26
Mane, Sridhar <Sridhar.Mane@RoswellPark.org>
2016-08-27 01:07:41 -0400

    Implemented menu for managing each cleansweep condensed the list show data as icon, removing times as they don't matter to admin in list

--- src/app/components/manage-clean-sweep/manage-clean-sweep.component.ts
+++ src/app/components/manage-clean-sweep/manage-clean-sweep.component.ts
@@ -1,9 +1,10 @@
-import { Component, OnInit, NgZone} from '@angular/core';
+import { Component, OnInit, NgZone, ViewChildren, QueryList} from '@angular/core';
 import { DataService } from '../../services/data.service';
 import { FirebaseListObservable } from 'angularfire2';
 import { StreetNamesPipe } from '../../pipes/street-names.pipe';
 import { CategorizeByDatePipe } from '../../pipes/categorize-by-date.pipe';
 import { MonthNamesPipe } from '../../pipes/month-names.pipe';
+import { MdMenuTrigger } from '@angular2-material/menu';
 
 
 @Component({
@@ -13,22 +14,56 @@ import { MonthNamesPipe } from '../../pipes/month-names.pipe';
   pipes: [StreetNamesPipe, CategorizeByDatePipe, MonthNamesPipe]
 })
 export class ManageCleanSweepComponent implements OnInit {
+  // @ViewChildren(MdMenuTrigger) triggers: QueryList<MdMenuTrigger>;
 
   cleanSweeps: FirebaseListObservable<any[]>;
-
+  currentKey: string = '';
 
 
   constructor(ds: DataService, private nz: NgZone) {
     this.cleanSweeps = ds.cleanSweeps;
+
+    // this.cleanSweeps.subscribe(() => {
+    // this.triggers.changes.subscribe((data) => {
+    //   console.log(data._results);
+    //   data._results.forEach((item:MdMenuTrigger) => {
+    //     // item.
+    //     item.onMenuClose.subscribe((data) => {
+    //       console.log('Closed, Got Data: ', data);
+
+    //     });
+    //   });
+    // });
+    // });
+
   }
 
-  delete(key: string) {
-    this.cleanSweeps.remove(key);
-    this.nz.run(() => {
-      console.log('Reloading !');
-    });
-  }
 
+  // delete(key: string) {
+
+  // }
+  selectedMenu(key) {
+    console.log(key);
+    this.currentKey = key;
+  }
+  edit() {
+    console.log('edit: ', this.currentKey);
+    
+  }
+  delete() {
+    console.log('delete: ', this.currentKey);
+    // this.cleanSweeps.remove(key);
+    // this.nz.run(() => {
+    //   console.log('Reloading !');
+    // });
+  }
   ngOnInit() {
+
+    // if (this.trigger) {
+    //   this.trigger.onMenuClose.subscribe((data) => {
+    //     console.log(data);
+    //   });
+    // }
   }
+
 }
